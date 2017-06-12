e7fda3a29503fd4f3a5ec1f79acf2da499ab3284
1884140140 <2293428954@qq.com>
2017-06-12 08:56:57 +0000

    Add files via upload

---
+++ HandleExit.java
@@ -0,0 +1,33 @@
+package myservlet.control;
+import mybean.data.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+@SuppressWarnings("serial")
+public class HandleExit extends HttpServlet{
+   public void init(ServletConfig config) throws ServletException{
+      super.init(config);
+   }
+   public  void  doPost(HttpServletRequest request,HttpServletResponse response) 
+                        throws ServletException,IOException{
+      HttpSession session=request.getSession(true); 
+      Login login=(Login)session.getAttribute("login"); 
+      boolean ok=true; 
+       if(login==null){
+         ok=false; 
+         response.sendRedirect("login.jsp"); 
+       }
+       if(ok==true)
+         continueDoPost(request,response);
+   }
+   public void continueDoPost(HttpServletRequest request,HttpServletResponse response) 
+                               throws ServletException,IOException{
+      HttpSession session=request.getSession(true); 
+      session.invalidate();              //�����û���session����
+      response.sendRedirect("index.jsp"); //������ҳ   
+   }
+   public  void  doGet(HttpServletRequest request,HttpServletResponse response) 
+                        throws ServletException,IOException{
+      doPost(request,response);
+   }
+}
